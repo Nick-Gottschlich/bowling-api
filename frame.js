607fe66aca2fc7524b36774c186c3be187022d64
Nicholas Gottschlich <nicholas.gottschlich@data.world>
2016-10-26 22:44:10 -0500

    got some classes defined tho

--- frame.js
+++ frame.js
@@ -1,6 +1,17 @@
 var express = require('express');
 //frame has a playerID,
 
-var frame = 10;
+class frame {
+  constructor(frameID) {
+    if (frameID === 10) {
+      this.test = "10th frame";
+    }
+    else {
+      this.test = "not 10th frame";
+    }
+  }
+}
+
+// let frame = "hi";
 
 module.exports = frame;
--- lane.js
+++ lane.js
@@ -6,21 +6,25 @@ var frame = require('./frame.js');
 class player {
 	constructor(id) {
 		this.id = id;
-		frame[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    this.frame = [];
+		for (let i = 1; i <= 10; i++) {
+      this.frame[i] = new frame(i);
+    }
 	}
 }
 
+playerTest = new player;
+
 router.get('/', function(req, res) {
-	res.send(frame)
+	res.send(playerTest.frame[10].test)
 })
 
+// what i should actually do is have an array of players, and this appends a new player to that array
 // creates a new player, ex: /player/steve creates a player with id steve (variable name for the player is also "player_steve")
 router.post('/player/:id', function(req, res){
-	//set the player name to the id
 	let playerName = "player_" + req.params.id;
-	res.send(playerName);
 	playerName = new player(req.params.id);
-	res.send (playerName.id);
+	//need to do something here or it won't exit, res.send("something");
 });
 
 module.exports = router;
